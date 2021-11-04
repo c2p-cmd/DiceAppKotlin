af99b45a1b2cbb1ca97bfb3b822a137d063d6246
c2p-cmd <sharandt19@live.com>
2021-11-04 12:03:36 +0530

    Added multithreading

--- src/main/kotlin/com/c2p/dicepracticek/DiceApplication.kt
+++ src/main/kotlin/com/c2p/dicepracticek/DiceApplication.kt
@@ -5,8 +5,9 @@ import javafx.fxml.FXMLLoader
 import javafx.scene.Scene
 import javafx.scene.image.Image
 import javafx.stage.Stage
+import kotlin.concurrent.thread
 
-class DiceApplication : Application() {
+class DiceApplication : Application(), Runnable {
     override fun start(stage: Stage) {
         val fxmlLoader = FXMLLoader(DiceApplication::class.java.getResource("app-layout.fxml"))
         val scene = Scene(fxmlLoader.load(), 420.0, 420.0)
@@ -22,8 +23,18 @@ class DiceApplication : Application() {
         stage.isResizable = false
         stage.show()
     }
+
+    override fun run() {
+        launch(DiceApplication::class.java)
+    }
 }
 
 fun main() {
-    Application.launch(DiceApplication::class.java)
+    thread(
+        start = true,
+        name = "Dice App Thread."
+    ) {
+        println("Currently Running Thread: ${Thread.currentThread().name}")
+        DiceApplication().run()
+    }
 }
